e342356a140ff3c18f25ec8583f0ce9fccdbd6eb
choi-sg-dev <78799894+choi-sg-dev@users.noreply.github.com>
2021-02-14 19:35:33 +0900

    test

--- README.md
+++
@@ -1,3 +0,0 @@
-# Portfolio
-
-포트폴리오 url : https://choi-sg-dev.github.io/Choi-Portfolio/
--- vue-todo/todo/vue.config.js
+++ vue-todo/todo/vue.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
     outputDir:'./docs',
     publicPath: '/vue-test/'
-}
+}*/
